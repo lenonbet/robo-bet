3ec0c98c6d93138eedc05f515eb7ea8698a5df2f
lenonbet <lenonmiranda1996@gmail.com>
2026-04-03 15:35:27 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 import requests
 
+# LIGAS QUE VOCÊ QUER (PRINCIPAIS)
+LIGAS_DESEJADAS = [
+    "Brazilian Serie A",
+    "Brazilian Serie B",
+    "Premier League",
+    "Spanish La Liga",
+    "Italian Serie A",
+    "French Ligue 1",
+    "Portuguese Primeira Liga",
+    "German Bundesliga",
+]
+
 def buscar_jogos_por_data(data):
     url = f"https://www.thesportsdb.com/api/v1/json/3/eventsday.php?d={data}&s=Soccer"
     
@@ -14,6 +26,10 @@ def buscar_jogos_por_data(data):
     for j in eventos:
         liga = j.get("strLeague", "Outros")
 
+        # FILTRA SÓ AS LIGAS IMPORTANTES
+        if liga not in LIGAS_DESEJADAS:
+            continue
+
         if liga not in ligas:
             ligas[liga] = []
 
@@ -25,3 +41,4 @@ def buscar_jogos_por_data(data):
         })
 
     return ligas
+  
