583291d26c96e8448430650f720883b7ef8aa426
lenonbet <lenonmiranda1996@gmail.com>
2026-04-03 14:10:42 -0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,26 @@
+import requests
+
+def buscar_jogos_por_data(data):
+    url = f"https://www.thesportsdb.com/api/v1/json/3/eventsday.php?d={data}&s=Soccer"
+    
+    try:
+        res = requests.get(url).json()
+        eventos = res.get("events", [])
+    except:
+        return {}
+
+    ligas = {}
+
+    for j in eventos:
+        liga = j.get("strLeague", "Outros")
+
+        if liga not in ligas:
+            ligas[liga] = []
+
+        ligas[liga].append({
+            "nome": f"{j['strHomeTeam']} x {j['strAwayTeam']}",
+            "casa": j['strHomeTeam'],
+            "fora": j['strAwayTeam']
+        })
+
+    return ligas
